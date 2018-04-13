bdc9afcd62db701b7836e6042fa9c514fb27fde9
sucun21 <suk3c2uit@gmail.com>
2018-04-13 09:29:44 +0700

    change sha256key

--- src/com/gbc/mc/common/AppConst.java
+++ src/com/gbc/mc/common/AppConst.java
@@ -13,8 +13,14 @@ public class AppConst {
     public static final int INFO_GENERIC        = 1;
     public static final int ERROR_GENERIC       = -1;
     public static final int NO_ERROR            = 0;
-    public static final String HMAC_SHA256_KEY  = "CYuE5J32xEfbrqois5DoowEwcv7Y2Yct";
+    
+    public static final String HMAC_SHA256_KEY  ="VeUgjaUjxkPbhFLekuXgzbimOLYvGKya";
     public static final String MID              = "14";
+    
+    //Mid test
+    //public static final String HMAC_SHA256_KEY  = "CYuE5J32xEfbrqois5DoowEwcv7Y2Yct";
+    //public static final String MID              = "7";
+    
     public static final String M_ZALOPAY_ID     = "160708000000502"; 
     public static final String M_ZALOPAY_P_IN   = "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92";
     public static final String TRANSFER_DESC    = "Chuyen tien test";    
